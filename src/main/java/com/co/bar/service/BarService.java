d3af3467915ca4c772476834c27290f30e655206
John Ochoa <jochoa@grupocinte.com>
2022-02-24 15:01:52 -0500

    primer commit

--- src/main/java/com/co/bar/service/BarService.java
+++ src/main/java/com/co/bar/service/BarService.java
@@ -2,6 +2,7 @@ package com.co.bar.service;
 
 import com.co.bar.dto.BarDto;
 import com.co.bar.entity.ArraysEntity;
+import com.co.bar.exception.NotFoundException;
 import com.co.bar.repository.BarRepository;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
@@ -27,7 +28,7 @@ public class BarService {
     private int count = 0;
 
     /**
-     * Verifica si un numero es primo
+     * Verifica si un numero es primo y lo agrega a una lista de numeros primos para ser retornado
      *
      * @param n
      * @return
@@ -48,6 +49,14 @@ public class BarService {
         return !IntStream.rangeClosed(2, number / 2).anyMatch(i -> number % i == 0);
     }
 
+    /**
+     * Retorna un objeto de tipo ResponseEntity BarDto para el caso
+     * de que el id de la pila no exista en la base de datos, va ha hacia la capa del controlador
+     *
+     * @param iteraciones
+     * @param idPilaDataBase
+     * @return
+     */
     public ResponseEntity<BarDto> getBar(int iteraciones, int idPilaDataBase) {
         var barDto = new BarDto();
 
@@ -58,7 +67,7 @@ public class BarService {
         Optional<ArraysEntity> bartender = repository.findById(idPilaDataBase);
         List<Integer> arrayResponse = new ArrayList<>();
         if (!bartender.isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            throw new NotFoundException("No se encontro el id de la pila en la base de datos");
         } else {
             List<Integer> listaNumeros = Arrays.stream(bartender.get().getInput_array().split(DELIMITER))
                     .sorted(Collections.reverseOrder())
@@ -69,16 +78,8 @@ public class BarService {
             List<Integer> arrayP = numeroPrimo(100);
 
             for (int x = 0; x < iteraciones; x++) {
-                for (Integer number : listaNumeros) {
-                    if (number % arrayP.get(x) == 0) {
-                        arrayB.add(number);
-                        arrayResponse.add(number);
-                    } else {
-                        arrayA.add(number);
-                    }
-                }
+                determinarElNumero(arrayResponse, listaNumeros, arrayA, arrayB, arrayP, x);
                 logArray(arrayA, arrayB);
-//                barDto.setMessage(logArray);
                 listaNumeros = new ArrayList<>(arrayA);
                 if (x + 1 < iteraciones) {
                     arrayA.clear();
@@ -90,14 +91,42 @@ public class BarService {
         }
 
         barDto.setResponseCode(200);
-
-
+        barDto.setMessage("Respuesta correcta, observa el log para ver los resultados =)");
         String respuesta = arrayResponse.stream().map(Object::toString).collect(Collectors.joining(DELIMITER));
         barDto.setRespuesta(respuesta);
         return ResponseEntity.status(HttpStatus.OK).body(barDto);
 
     }
 
+    /**
+     * Determina si el número
+     * del plato es divisible uniformemente por el Pi número primo. Si lo es, agréguelo en un arreglo B.
+     * En caso contrario, añádalo a un arreglo Ai
+     *
+     * @param arrayResponse
+     * @param listaNumeros
+     * @param arrayA
+     * @param arrayB
+     * @param arrayP
+     * @param x
+     */
+    private void determinarElNumero(List<Integer> arrayResponse, List<Integer> listaNumeros, List<Integer> arrayA, List<Integer> arrayB, List<Integer> arrayP, int x) {
+        for (Integer number : listaNumeros) {
+            if (number % arrayP.get(x) == 0) {
+                arrayB.add(number);
+                arrayResponse.add(number);
+            } else {
+                arrayA.add(number);
+            }
+        }
+    }
+
+    /**
+     * Log de los arreglo A y B para ver el resultado e imprimirlo en la consola
+     *
+     * @param arrayA
+     * @param arrayB
+     */
     private void logArray(List<Integer> arrayA, List<Integer> arrayB) {
         count++;
         String arrayAA = arrayA.stream().map(Object::toString).collect(Collectors.joining(DELIMITER));
@@ -109,6 +138,8 @@ public class BarService {
 
 
     /**
+     * Retorna un objeto de tipo BarDto con el código de respuesta y el mensaje
+     *
      * @param barDto
      * @return
      */
